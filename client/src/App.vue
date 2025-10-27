e4206aa03a4fda771b5cd3560404682ab7fe8a76
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-10-28 04:15:34 +0800

    persist user session accross page changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,19 +2,26 @@
 import { RouterView } from 'vue-router'
 import Header from '@/components/Header.vue'
 import { useRoute } from 'vue-router'
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
+import { useAuth } from '@/composables/useAuth'
 
 const route = useRoute()
-
+const { initializeUser } = useAuth()
 const showLayout = computed(() => {
   return route.name !== 'home' && route.name !== 'login'
+})
+onMounted(() => {
+  initializeUser()
 })
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col">
     <Header v-if="showLayout" />
-    <div v-if="showLayout" class="flex-1 border-l border-r border-gray-200 mx-auto w-full max-w-[90rem]">
+    <div
+      v-if="showLayout"
+      class="flex-1 border-l border-r border-gray-200 mx-auto w-full max-w-[90rem]"
+    >
       <main class="container mx-auto font-display py-8 px-20 h-full">
         <RouterView />
       </main>
