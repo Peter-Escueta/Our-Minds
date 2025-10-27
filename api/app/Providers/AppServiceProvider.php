b637f3ed20847e024a8b054da2e6488960bb5703
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-10-28 01:04:21 +0800

    All setup changes in relation to transforming to Laravel Passport

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -11,7 +11,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // in case id ever want to adjust token expiration, refresh token expiration, and personal access token expiration
+        //Passport::tokensExpireIn(CarbonInterval::days(amountOfDays));
+        //Passport::refreshTokensExpireIn(CarbonInterval::days(amountOfDays));
+        //Passport::personalAccessTokensExpireIn(CarbonInterval::days(amountOfDays));
     }
 
     /**
