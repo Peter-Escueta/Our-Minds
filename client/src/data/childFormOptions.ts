4330a647ebb78d113c7b9100e19c310b21a88598
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-11-01 03:52:05 +0800

    added constant data folders

---
+++ client/src/data/childFormOptions.ts
@@ -0,0 +1,30 @@
+
+export const genderOptions = [
+  { label: 'Male', value: 'male'},
+  { label: 'Female', value: 'female'},
+  { label: 'Other', value: 'other'},
+]
+export const gradeOptions =  [
+'Pre-K',
+'Kindergarten',
+'1st',
+'2nd',
+'3rd',
+'4th',
+'5th',
+'6th',
+'7th',
+'8th',
+'9th',
+'10th',
+'11th',
+'12th',
+]
+
+export const placementOptions = [
+  'Regular Class',
+  'Special Education Class',
+  'Inclusion Program',
+  'Home School',
+  'Other',
+]
---
+++ client/src/data/evaluationRecommendations.ts
@@ -0,0 +1,11 @@
+export const standardRecommendations = [
+  'Educational/school placement - Special Education Special Class (Minimum of 3 hours, 5 times a week)',
+  'Design and implement a whole-year Individualized Educational Program',
+  'Create a Team of professionals (SpED Team) to address cognitive and behavioral difficulties',
+  'Occupational/Behavioral therapy - minimum of 2 times a week',
+  'Speech Therapy - focus on expressive language pragmatics',
+  'Parent & Immediate caregiver Direct Involvement in the Special Education program',
+  'Family reorientation concerning methodologies for home-based behavior modifications',
+  'Increase self-help skills through partnership of auxiliary therapist/services & family intervention',
+  'Increase psychosocial exposure like going to the mall and increase interaction with children',
+]
