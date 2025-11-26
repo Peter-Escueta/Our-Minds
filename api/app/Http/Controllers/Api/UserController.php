a129a6bc38e6e3b957de680e9ae1ba3691956af3
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-11-27 02:35:30 +0800

    user deletion

--- api/app/Http/Controllers/Api/UserController.php
+++ api/app/Http/Controllers/Api/UserController.php
@@ -13,16 +13,29 @@ class UserController extends Controller
         $users = User::all();
         return response()->json($users);
     }
+    public function destroy(User $user)
+    {
+        try {
+            $user->delete();
+            return response()->json($user);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'User deletion failed', 'error' => $e->getMessage()], 500);
+        }
+    }
     public function store(Request $request)
     {
         try {
             $validated = $request->validate([
                 'email' => 'required|email|unique:users,email',
                 'password' => 'required|string',
+                'role' => 'required|string',
+                'name' => 'required|string'
             ]);
             $user = User::create([
                 'email' => $validated['email'],
-                'password' => bcrypt($validated['password'])
+                'password' => bcrypt($validated['password']),
+                'role' => $validated['role'],
+                'name' => $validated['name']
             ]);
             return response()->json($user, 201);
         } catch (\Exception $e) {
