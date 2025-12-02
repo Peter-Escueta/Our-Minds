8002673e76a1a9cad1af9ce2eefc5f2c648301ad
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-12-02 22:37:58 +0800

    added websites and status removed summary_notes

--- api/database/migrations/2025_07_30_072739_create_asssessment_evaluations_table.php
+++ api/database/migrations/2025_07_30_072739_create_asssessment_evaluations_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\EvaluationStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,11 +15,14 @@ return new class extends Migration
         Schema::create('assessment_evaluations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('assessment_id')->constrained()->cascadeOnDelete();
-            $table->text('background_information');
-            $table->json('recommendations');
-            $table->text('summary_notes');
-            $table->timestamps(); 
-            
+            $table->text('background_information')->nullable();
+            $table->json('recommendations')->nullable();
+            $table->text('websites')->nullable();
+            $values = array_column(EvaluationStatus::cases(), 'value');
+            $table->enum('status', $values)
+                ->default(EvaluationStatus::READY_FOR_EVALUATION->value);
+            $table->timestamps();
+
             $table->index('assessment_id');
         });
     }
