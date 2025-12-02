4077e649ab1fc427d54d4e391601e9c771a41bc9
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-12-02 22:38:16 +0800

    correct plural naming

--- api/database/migrations/2025_11_13_105603_create_therapy_table.php
+++ api/database/migrations/2025_11_13_105603_create_therapy_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('therapy', function (Blueprint $table) {
+        Schema::create('therapies', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->foreignId('child_id')->constrained('children')->onDelete('cascade');
