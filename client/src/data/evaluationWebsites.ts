53208bd904ae9faee487f05da1a1534aa8ccc011
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2025-12-02 22:33:59 +0800

    static website data

---
+++ client/src/data/evaluationWebsites.ts
@@ -0,0 +1,27 @@
+export const standardEvaluationWebsites = [
+  // --- Philippines Specific Organizations (NGOs & Support) ---
+  'autismsocietyphilippines.org',   // Autism Society Philippines (ASP)
+  'adhdsociety.ph',                 // ADHD Society of the Philippines
+  'dsapi.org',                      // Down Syndrome Association of the Philippines
+  'blind.org.ph',                   // Resources for the Blind, Inc.
+  'norfil.org',                     // Norfil Foundation (Vulnerable children & disability support)
+  'specialolympicspilipinas.org',   // Special Olympics Pilipinas (Sports & Inclusion)
+  'kythe.org',                      // Kythe Foundation (Psychosocial support for chronic illness)
+  'unilabfoundation.org',           // Project Inclusion (Inclusive employment for PWDs)
+
+  // --- Philippine Government Agencies (Rights, Benefits, PWD ID) ---
+  'ncda.gov.ph',                    // National Council on Disability Affairs (Main agency for PWD rights)
+  'dswd.gov.ph',                    // Dept. of Social Welfare and Development (Social services)
+  'deped.gov.ph',                   // Dept. of Education (For SPED enrollment/Learner Reference Number)
+  'cwc.gov.ph',                     // Council for the Welfare of Children
+
+  // --- International / General Resources (Valid in PH Context) ---
+  'autism.org',                     // Autism Research Institute (Medical/Ed research)
+  'childmind.org',                  // Child Mind Institute (Mental health & learning disorders guide)
+  'understood.org',                 // Understood (Practical guides for learning differences)
+  'cdc.gov/ncbddd/autism',          // CDC Milestones (Standard developmental benchmarks)
+  'who.int/health-topics/disability', // World Health Organization
+  'bookshare.org',                  // Accessible online library (works globally)
+  'chadd.org',                      // CHADD (Leading resource for ADHD info)
+  'researchautism.org',             // Organization for Autism Research (OAR)
+]
